0f860d1aa1a86a317cf998db8e1b0c24b4dd266d
zhu571 <zhuhaofan@impcas.ac.cn>
2026-07-24 20:09:49 +0800

    fix: 同步前端dist

---
+++ go-server/static/assets/useNotify-CMocqCf8.js
@@ -0,0 +1,1 @@
+import{k as t}from"./vendor-element-C60KORvp.js";import"./vendor-BFS-rBJR.js";function m(e,o){const s=e,r=(s==null?void 0:s.message)||o;t.error(s!=null&&s.requestId?`${r}（request_id: ${s.requestId}）`:r)}export{m as s};
